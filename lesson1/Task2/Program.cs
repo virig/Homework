327557368ec24e9ba0d501136640caced4fd7a13
virig <virig@yandex.ru>
2022-04-24 22:31:36 +0500

    Задача 2.(ошибка )

---
+++ lesson1/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = 2, b = 3, c = 7;
+
+if ( a > b && b > c ) Console.WriteLine("Максимальное число = "  + a );
+
+else if ( b > c ) Console.WriteLine("Максимальное число = "  + b );
+     
+if ( c ) Console.WriteLine("Максимальное число = "  + c );
+  
+Console.ReadLine();
