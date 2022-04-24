71c3d3550e4d83a59c5e92447caa48df6212e0f8
virig <virig@yandex.ru>
2022-04-24 21:15:29 +0500

    Решение первой задачи !!!

--- lesson1/Task1/Program.cs
+++ lesson1/Task1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a = -9, b = -3;
+    if ( a > b )
+        Console.WriteLine("Максимальное число = " + a );
+    else
+        Console.WriteLine("Максимальное число = " + b );
+
+Console.ReadLine();
+
