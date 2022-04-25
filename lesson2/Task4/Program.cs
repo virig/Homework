fb609b43666e1b94d5fa2a99f60d0e5432720a18
virig <virig@yandex.ru>
2022-04-25 21:59:48 +0500

    Урок 2. Задача 4.

---
+++ lesson2/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int number = new Random().Next(1, 999);
+int a = 7;
+int b = 23;
+
+string txtResult = " ";
+
+if (number % a == 0 && number % b == 0) txtResult = "Кратно обоим числам";
+else if (number % a != 0 && number % b !=0) txtResult = "не кротно ни одному из чисел";
+else if (number % a == 0) txtResult = $"кратно только числу {a}";
+else txtResult = $"кратно только числу {b}";
+
+System.Console.WriteLine($"Проверить кратность чисел {a} и {b} ");
+System.Console.WriteLine();
+System.Console.WriteLine($"Числа {number} {txtResult} ");
+System.Console.WriteLine();
