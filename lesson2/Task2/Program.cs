71da82b8c79012d20fff7ace4c23f40b72417ea0
virig <virig@yandex.ru>
2022-04-25 21:34:35 +0500

    Урок 2. Задача 2.

---
+++ lesson2/Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Clear();
+System.Console.WriteLine();
+int number = new Random().Next(100, 999);
+
+System.Console.WriteLine($"Вторая цифра числа {number} является {number / 10 % 10} ");
+
+System.Console.WriteLine(); 
