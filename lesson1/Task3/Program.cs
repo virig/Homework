80089c685dc45bf7241b1715c08f2bd241a9d4aa
virig <virig@yandex.ru>
2022-04-25 20:58:41 +0500

    Задача 3.

---
+++ lesson1/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+System.Console.WriteLine();
+System.Console.Write("Введите число:  ");
+string writeNumber = Console.ReadLine();
+int number = Convert.ToInt32(writeNumber);
+string result = $"Число {number} является чётным";
+
+if (number % 2 != 0) result = result.Replace("является", "не является");
+
+System.Console.WriteLine(result);
+System.Console.WriteLine();
+
+
