dcf3184c700677e16881ed5b3f43dbf52f29f213
Yuri <74725537+virig@users.noreply.github.com>
2022-05-16 20:58:45 +0500

    Add files via upload

---
+++ lesson4/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+int number = 3;
+int exponent = 7;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
