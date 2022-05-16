53ebedde4f10d05670649a0e882a047f4d35091e
Yuri <74725537+virig@users.noreply.github.com>
2022-05-16 21:01:13 +0500

    Add files via upload

---
+++ lesson5/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Показать количество четных чисел в массиве, заполнеными случайными положительными трёхзначными числами. 
+ 
+Console.Clear();
+System.Console.WriteLine();
+
+int size = 20;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintResult(int[] array)
+{
+    int even = 0;
+     for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) even++;
+
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Количество четных чисел {even}.");
+    System.Console.WriteLine();
+}
+
+PrintResult(FillArray(size));
+
+
+
+// int[] CountEvenUneven(int[] array)                   // Это оказалось не нужным, но пусть пока будет
+// {
+//     int[] evenUneven = new int[2];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0) evenUneven[0]++;
+//         else evenUneven[1]++;
+//     }
+//     return evenUneven;
+// }
---
+++ lesson5/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// В массиве вещественных чисел, найти разницу между макс. и мин. элементов массива
+
+Console.Clear();
+System.Console.WriteLine();
+
+int size = new Random().Next(1, 5);
+
+System.Console.WriteLine($"Количетсов элементов массива: {size}");
+
+double[] FillSourceArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiffMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    System.Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    System.Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    System.Console.WriteLine();
+}
+
+PrintDiffMinMax(FillSourceArray(size));
+System.Console.WriteLine();
